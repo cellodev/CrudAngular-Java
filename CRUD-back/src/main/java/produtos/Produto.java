10b644ec6cfffc5f68f21efff8fa0d62f8357852
Marcelo Michels <58716558+cellodev@users.noreply.github.com>
2020-06-25 21:11:45 -0300

    Add files via upload

---
+++ CRUD-back/src/main/java/produtos/Produto.java
@@ -0,0 +1,54 @@
+package produtos;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "produtos", schema = "public")
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+    private Double preco;
+    
+    public Produto() {
+    }
+
+    public Produto(Integer id, String descricao, Double preco) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
